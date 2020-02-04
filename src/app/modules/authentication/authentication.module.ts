57695eb4f5406053b966f2a6bc48ecb63abfe11c
kwamekert <tomriddle477@gmail.com>
2020-02-04 16:02:21 +0000

    Flex working

--- src/app/modules/authentication/authentication.module.ts
+++ src/app/modules/authentication/authentication.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     AuthenticationRoutingModule,
-    CommonModule
+    CommonModule,MatCardModule,FlexLayoutModule
   ]
 })
 export class AuthenticationModule { }
