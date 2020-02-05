f61e9432dcd4ba80874b7cf986bbe66f7710d01b
kwamekert <tomriddle477@gmail.com>
2020-02-05 16:44:56 +0000

    Sidebar fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { HeaderComponent ,SidebarComponent,FooterComponent} from './layouts';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { DefaultComponent } from './layouts/default/default.component';
-import {MatToolbarModule} from '@angular/material/toolbar'
-import {  MatSidenavModule, MatDividerModule, MatCardModule, MatTableModule, MatPaginatorModule } from '@angular/material';
-import {MatButtonModule} from '@angular/material/button';
-import { MatMenuModule} from '@angular/material/menu';
+//import {MatToolbarModule} from '@angular/material/toolbar'
+//import {  MatSidenavModule, MatDividerModule, MatCardModule, MatTableModule, MatListModule,MatPaginatorModule } from '@angular/material';
+import {  MatDividerModule, MatToolbarModule, MatIconModule, MatButtonModule, 
+  MatMenuModule, MatListModule } from '@angular/material';
+  
+import { MatSidenavModule,  MatCardModule, MatTableModule, MatPaginatorModule} from '@angular/material';
+//import {MatButtonModule} from '@angular/material/button';
+//import { MatMenuModule} from '@angular/material/menu';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatIconModule } from '@angular/material/icon';
+//import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -22,7 +26,6 @@ import { MatIconModule } from '@angular/material/icon';
     SidebarComponent,
     FooterComponent,
     DefaultComponent,
-    
 
   ],
   imports: [
@@ -31,14 +34,15 @@ import { MatIconModule } from '@angular/material/icon';
     AppRoutingModule,
     MatToolbarModule,
     MatIconModule,
-    MatSidenavModule,
     MatDividerModule,
     FlexLayoutModule,
-    MatCardModule,
-    MatTableModule,
-    MatPaginatorModule,
     MatMenuModule,
-    MatButtonModule
+    MatButtonModule,
+    MatListModule,
+    MatSidenavModule,  
+    MatCardModule, 
+    MatTableModule, 
+    MatPaginatorModule
     
   ],
   providers: [],
